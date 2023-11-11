d230ed4870657d217febf9c5aaca72897d1a28d6
William Tshani <williamtshani@gmail.com>
2023-11-11 07:42:51 +0200

    completed w02 task

---
+++ w02-assignment/scripts/w02-task.js
@@ -0,0 +1,51 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullName = "William Tshani";
+const currentYear = 2023;
+const profilePicture = "images/me.png";
+
+
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector("img");
+
+
+
+
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = `${currentYear}`;
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", `Profile image of ${profilePicture}`);
+
+
+
+
+
+
+/* Step 5 - Array */
+const favoriteFoods = ["Meat", " Veggies", " Fruits", " Fish", " Pasta", " Rice", " desserts"];
+foodElement.textContent = `${favoriteFoods}`;
+
+const anotherFavoriteFood = " Junk food";
+favoriteFoods.push(anotherFavoriteFood);
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.shift();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.pop();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+
+
+
+
+
+
